e6e5e25f59ca2793da3250d55a000a7a49f87115
Christian Kyle Valdez <christiankyle.valdez@g.batstate-u.edu.ph>
2022-11-03 21:01:15 +0800

    commit part 2

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include('views/includes/head.php'); ?>
+</head>
+
+<body>
+
+    <?php include('views/login/login.php'); ?>
+
+    <footer>
+        <?php include('views/includes/footer.php'); ?>
+    </footer>
+
+</body>
+
+</html>
---
+++ views/login/login.php
@@ -0,0 +1,5 @@
+<p class="woof"> Hi Woof! </p>
+<p class="welcome">welcome back</p>
+<input class="username" placeholder="Username">
+<input class="password" placeholder="Password">
+<button class="login_btn"> Log In </button>
