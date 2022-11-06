9c521311bc2ef0071f0759a9ad929083188e427c
Christian Kyle Valdez <christiankyle.valdez@g.batstate-u.edu.ph>
2022-11-06 12:25:17 +0800

    commit

--- index.php
+++ index.php
@@ -5,13 +5,28 @@
     <?php include('views/includes/head.php'); ?>
 </head>
 
-<body>
+<body class="bg-gradient-primary">
 
-    <?php include('views/login/login.php'); ?>
+    <?php if (isset($_SESSION['randomId'])) : ?>
+        <?php include('views/dashboard/dashboard.php'); ?>
+    <?php else : ?>
+        <?php include('views/login/login.php'); ?>
+    <?php endif; ?>
 
-    <footer>
-        <?php include('views/includes/footer.php'); ?>
-    </footer>
+    <?php if (isset($_SESSION['randomId'])) : ?>
+        <footer class="sticky-footer bg-white">
+            <div class="container my-auto">
+                <div class="copyright text-center my-auto">
+                    <span>Copyright © 2022. All Rights Reserved.</span>
+                </div>
+            </div>
+            <?php include('views/includes/footer.php'); ?>
+        </footer>
+    <?php else : ?>
+        <footer>
+            <?php include('views/includes/footer.php'); ?>
+        </footer>
+    <?php endif; ?>
 
 </body>
 
