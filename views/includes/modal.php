5fb3c484d54daed0d6ca2a9580802fc52d3daf37
RIMAS, DANIELLA YVETTE D <daniellayvetterimas@gmail.com>
2022-11-26 20:02:15 +0800

    user

--- views/includes/modal.php
+++ views/includes/modal.php
@@ -381,3 +381,38 @@
     </div>
   </div>
 </div>
+
+<!-- User Modal -->
+<div class="modal fade" id="userModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Add User Account</h5>
+          <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">×</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <form role="form" method="post" id="addUser">
+            <div class="form-group">
+                <select class='form-control' id='prodSupplier'>
+                  <option value="" disabled selected hidden>Select Employee</option>
+                  <option value="0">Male</option>
+                  <option value="1">Female</option>
+                </select>
+              </div>
+              <div class="form-group">
+                <input class="form-control" placeholder="Username" name="username" required>
+              </div>
+              <div class="form-group">
+                <input type="password" class="form-control" placeholder="Password" name="password" required>
+              </div>
+            <hr>
+            <button type="submit" class="btn btn-success"><i class="fa fa-check fa-fw"></i>Save</button>
+            <button type="reset" class="btn btn-danger"><i class="fa fa-times fa-fw"></i>Reset</button>
+            <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>      
+          </form>  
+        </div>
+      </div>
+    </div>
+  </div>
